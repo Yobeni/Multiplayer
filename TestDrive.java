c99cf317a18c37b7b07f73f3931ba1f3ac0051d2
crodg1023 <crodg1023@outlook.com>
2023-03-27 16:12:12 +0200

    prueba for

--- TestDrive.java
+++ TestDrive.java
@@ -2,5 +2,9 @@ public class TestDrive {
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("Buenas tardes");
+
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("Numero " + i);
+        }
     }
 }
