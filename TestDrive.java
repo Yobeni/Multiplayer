226b6555da8b9beb257e9599e09a1c723eb2d6af
crodg1023 <crodg1023@outlook.com>
2023-03-27 16:25:24 +0200

    prueba 3 :D

--- TestDrive.java
+++ TestDrive.java
@@ -7,6 +7,13 @@ public class TestDrive {
             System.out.println("Numero " + i);
         }
         test();
+        Persona p1 = new Persona();
+        Persona p2 = new Persona();
+        p1.setEdad(18);
+        p2.setEdad(25);
+
+        int suma = sumar(p1.getEdad(), p2.getEdad());
+        System.out.println(suma);
     }
     public static void test() {
         System.out.println("tests");
@@ -14,7 +21,4 @@ public class TestDrive {
     public static int sumar(int a, int b) {
         return a+b;
     }
-    private String name;
-
-    Persona p1 = new Persona();
 }
