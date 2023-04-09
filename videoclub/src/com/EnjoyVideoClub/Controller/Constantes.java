736458a9e32d20cbb7b247768e1553e216e8303f
crodg <crodg1023@outlook.com>
2023-04-09 18:04:28 +0200

    actualización de los nombres de las variables constantes.

--- videoclub/src/com/EnjoyVideoClub/Controller/Constantes.java
+++ videoclub/src/com/EnjoyVideoClub/Controller/Constantes.java
@@ -4,9 +4,9 @@ package com.EnjoyVideoClub.Controller;
 import java.util.Date;
 
 public class Constantes {
-    public static final String defaultNIF = "";
-    public static final String defaultNombre = "";
-    public static final String defaultPoblacion = "";
-    public static final Date defaultFechaNac = new Date(2005-01-01);
+    public static final String NIF_DEFAULT = "";
+    public static final String NOMBRE_DEFAULT = "";
+    public static final String POBLACION_DEFAULT = "";
+    public static final Date FECHA_NACIMIENTO_DEFAULT = new Date(2005- 1- 1);
 
 }
