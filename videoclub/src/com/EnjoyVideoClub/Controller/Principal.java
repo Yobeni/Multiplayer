56a038a913c2a93e9863f7e2d5568976b42fed84
crodg <crodg1023@outlook.com>
2023-04-25 11:23:48 +0200

    Creación del apartado gráfico "añadir Videojuego" v1.0.

--- videoclub/src/com/EnjoyVideoClub/Controller/Principal.java
+++ videoclub/src/com/EnjoyVideoClub/Controller/Principal.java
@@ -1,7 +1,11 @@
 package com.EnjoyVideoClub.Controller;
 
+import com.EnjoyVideoClub.Views.CrearVideojuegoGUI;
+
 public class Principal {
     public static void main(String[] args) {
         System.out.println("CLASE PRINCIPAL");
+
+        new CrearVideojuegoGUI();
     }
 }
