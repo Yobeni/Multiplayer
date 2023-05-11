b041f1e39d8319437b95c2517e37c468c9c053da
crodg1023 <crodg1023@outlook.com>
2023-05-11 16:46:07 +0200

    Se agrega la interfaz gráfica de la ventana main,y de la ventana de altas.

--- videoclub/src/com/EnjoyVideoClub/Controller/Principal.java
+++ videoclub/src/com/EnjoyVideoClub/Controller/Principal.java
@@ -1,17 +1,10 @@
 package com.EnjoyVideoClub.Controller;
-
-import com.EnjoyVideoClub.Model.*;
-import com.EnjoyVideoClub.Views.CrearVideojuegoGUI;
 import com.EnjoyVideoClub.Views.VentanaMainGUI;
-
-import java.awt.*;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
         new VentanaMainGUI();
+
+        System.out.println("Ya tenemos ventana main de GUI");
     }
 }
