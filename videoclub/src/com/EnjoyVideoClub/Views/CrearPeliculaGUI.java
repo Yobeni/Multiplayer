591c333ad85991499d8bac2360b999b965d99a73
Yobeni <joabenmar@alu.edu.gva.es>
2023-05-11 18:40:55 +0200

    Modificación GUI Pelicula

--- videoclub/src/com/EnjoyVideoClub/Views/CrearPeliculaGUI.java
+++ videoclub/src/com/EnjoyVideoClub/Views/CrearPeliculaGUI.java
@@ -1,10 +1,12 @@
 package com.EnjoyVideoClub.Views;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
-public class CrearPeliculaGUI {
+public class CrearPeliculaGUI extends JFrame{
     private JPanel CrearPeliculaPanel;
     private JTextField textField1;
     private JTextField textField2;
@@ -23,12 +25,26 @@ public class CrearPeliculaGUI {
     private JLabel durecionLbl;
     private JLabel actorLbl;
     private JLabel actrizLbl;
+    private JButton retrocederBtn;
 
     public CrearPeliculaGUI() {
+        this.setContentPane(CrearPeliculaPanel);
+        this.setVisible(true);
+        this.setSize(700,500);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
         CrearPeliculaPanel.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
                 super.componentResized(e);
+            }
+        });
+        retrocederBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new altasGUI();
+                dispose();
             }
         });
     }
