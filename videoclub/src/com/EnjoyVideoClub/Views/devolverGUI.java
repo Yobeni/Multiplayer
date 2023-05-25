a62e20f1182b002890615f858c1c07f5b5643b6e
Yobeni <joabenmar@alu.edu.gva.es>
2023-05-25 19:06:02 +0200

    ComboBox devolverGUI

--- videoclub/src/com/EnjoyVideoClub/Views/devolverGUI.java
+++ videoclub/src/com/EnjoyVideoClub/Views/devolverGUI.java
@@ -1,15 +1,17 @@
 package com.EnjoyVideoClub.Views;
 
 import com.EnjoyVideoClub.Controller.Principal;
+import com.EnjoyVideoClub.Model.Alquiler;
+import com.EnjoyVideoClub.Model.Multimedia;
+import com.EnjoyVideoClub.Model.Pelicula;
+import com.EnjoyVideoClub.Model.Videojuego;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
+import java.awt.event.*;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class devolverGUI extends VentanaMainGUI {
@@ -31,6 +33,7 @@ public class devolverGUI extends VentanaMainGUI {
     private JTextField devolucionTF;
     private JTextField nifTf;
     private JLabel lbldevolucion;
+    private JComboBox tituloCBO;
 
     public devolverGUI() {
         Color backgroundColor = new Color(255, 222, 89);
@@ -44,6 +47,7 @@ public class devolverGUI extends VentanaMainGUI {
         this.setVisible(true);
 
         fechaActual();
+        mostrarDatosMultimediasDependiendoDelTipo();
 
         TtipoCBO.addItem("Pelicula");
         TtipoCBO.addItem("Videojuego");
@@ -70,27 +74,7 @@ public class devolverGUI extends VentanaMainGUI {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    int errores = 0;
-                    if (!nombreTF.equals("")&&!nifTf.equals("")){
-                        for (int i = 0;i< Principal.multimedias.size();i++){
-                            if (!Principal.multimedias.get(i).getTitulo().equals(nombreTF)){
-                                errores++;
-                                throw new RuntimeException("No pueden haber campos vacios.");
-                            }
 
-                        }
-                    }else {
-                        throw new RuntimeException("No pueden haber campos vacios.");
-                    }
-
-                    if (errores == 0){
-                        for (int i = 0;i<Principal.multimedias.size();i++){
-                            if (Principal.multimedias.get(i).getTitulo().equals(nombreTF)){
-                                alquilerTF.setText(Principal.multimedias.get(i).getAño().toString());
-                            }
-                        }
-                        JOptionPane.showMessageDialog(null, "Los datos son correctos");
-                    }
                 }catch (Exception ex){
                     JOptionPane.showMessageDialog(null, ex.getMessage());
                 }
@@ -115,4 +99,59 @@ public class devolverGUI extends VentanaMainGUI {
             ex.printStackTrace();
         }
     }
+
+    public void mostrarDatosMultimediasDependiendoDelTipo() {
+        ArrayList<Date> alquilerDates = new ArrayList<>();
+
+
+        TtipoCBO.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                tituloCBO.removeAllItems();
+                alquilerDates.clear();
+
+                int selectedIndex = TtipoCBO.getSelectedIndex();
+                if (selectedIndex == 0) {
+                    for (Alquiler alq : Principal.alquileres) {
+                        if (alq.getTipoMultimedia() instanceof Pelicula) {
+                            tituloCBO.addItem(alq.getTituloMultimedia());
+                            alquilerDates.add(alq.getFechaInicio());
+                        }
+                    }
+                } else if (selectedIndex == 1) {
+                    for (Alquiler alq : Principal.alquileres) {
+                        if (alq.getTipoMultimedia() instanceof Videojuego) {
+                            tituloCBO.addItem(alq.getTituloMultimedia());
+                            alquilerDates.add(alq.getFechaInicio());
+                        }
+                    }
+                }
+            }
+        });
+
+        tituloCBO.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                int selectedIndex = tituloCBO.getSelectedIndex();
+                if (TtipoCBO.getSelectedIndex() == 0 && selectedIndex >= 0 && selectedIndex < alquilerDates.size()) {
+                    try{
+                        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+                        String fechaString = formato.format(alquilerDates.get(selectedIndex));
+                        alquilerTF.setText(fechaString);
+                    }catch (Exception ex){
+                        ex.printStackTrace();
+                    }
+                } else if (TtipoCBO.getSelectedIndex() == 1 && selectedIndex >= 0 && selectedIndex < alquilerDates.size()) {
+                    try {
+                        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+                        String fechaString = formato.format(alquilerDates.get(selectedIndex));
+                        alquilerTF.setText(fechaString);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
 }
+
