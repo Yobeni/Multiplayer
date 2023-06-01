bbf7486448a326d382eae4f3f123191ece9ce2e4
crodg1023 <crodg1023@outlook.com>
2023-06-01 16:59:16 +0200

    colores :D

--- videoclub/src/com/EnjoyVideoClub/Views/altasGUI.java
+++ videoclub/src/com/EnjoyVideoClub/Views/altasGUI.java
@@ -1,8 +1,11 @@
 package com.EnjoyVideoClub.Views;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class altasGUI extends VentanaMainGUI{
     private JPanel panel1;
@@ -24,6 +27,69 @@ public class altasGUI extends VentanaMainGUI{
         this.setVisible(true);
 
         agregarmenu();
+        colores();
+        nuevasVentanas();
+    }
+
+    public void colores() {
+        Color naranja = new Color(250, 149, 18);
+        Color naranjaOscuro = new Color(253, 84, 27);
+
+        altaJuegoBtn.setBackground(naranja);
+        altaPeliBtn.setBackground(naranja);
+        altaCancionBtn.setBackground(naranja);
+        atrasBtn.setBackground(naranja);
+
+        altaJuegoBtn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                altaJuegoBtn.setBackground(naranjaOscuro);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                altaJuegoBtn.setBackground(naranja);
+            }
+        });
+
+        altaPeliBtn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                altaPeliBtn.setBackground(naranjaOscuro);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                altaPeliBtn.setBackground(naranja);
+            }
+        });
+
+        altaCancionBtn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                altaCancionBtn.setBackground(naranjaOscuro);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                altaCancionBtn.setBackground(naranja);
+            }
+        });
+
+        atrasBtn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                atrasBtn.setBackground(naranjaOscuro);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                atrasBtn.setBackground(naranja);
+            }
+        });
+    }
+
+    public void nuevasVentanas() {
         altaJuegoBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
